deded6135955592fe9157706485c53cf80e1a1a1
Reavar14 <faizulahmad09@gmail.com>
2026-06-17 02:59:20 +0700

    fix: update axios baseURL

--- backend/config/cors.php
+++ backend/config/cors.php
@@ -20,7 +20,7 @@ return [
     'allowed_methods' => ['*'],
 
     // Pastikan URL TIDAK ADA garis miring di akhir
-    'allowed_origins' => [env('FRONTEND_URL', 'https://catatankeuangan-production-6814.up.railway.app')],
+    'allowed_origins' => ['*'],
 
     'allowed_origins_patterns' => [],
 
